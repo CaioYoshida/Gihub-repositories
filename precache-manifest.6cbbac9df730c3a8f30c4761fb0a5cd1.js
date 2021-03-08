7952f57e4fbe95e4c2535796bffe25f674ae264f
Caio Yoshida Dell G3 <caio.yoshida@yahoo.com.br>
2021-03-07 21:53:56 -0300

    Updates

---
+++ precache-manifest.6cbbac9df730c3a8f30c4761fb0a5cd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72ce2360f785aeeb8fb122dbef6b707d",
+    "url": "/Gihub-repositories/index.html"
+  },
+  {
+    "revision": "c30d168e0e1a3419f27a",
+    "url": "/Gihub-repositories/static/js/2.929f845f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Gihub-repositories/static/js/2.929f845f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "218a9bc09051f8d9e391",
+    "url": "/Gihub-repositories/static/js/main.f799c285.chunk.js"
+  },
+  {
+    "revision": "b94430637c195b06af19",
+    "url": "/Gihub-repositories/static/js/runtime-main.bca4bc29.js"
+  },
+  {
+    "revision": "d5cc199a43f2c27b7f4aa48998159616",
+    "url": "/Gihub-repositories/static/media/logo.d5cc199a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.1116a58715572e50288e3865864d47dd.js"
+  "/Gihub-repositories/precache-manifest.6cbbac9df730c3a8f30c4761fb0a5cd1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Gihub-repositories/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
